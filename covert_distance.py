0a23acab642e2835ce8758b342fa9307ad9aa5e8
nikhitha126 <mandlanikhitha@gmail.com>
2024-12-18 21:46:32 +0530

    Add files via upload

---
+++ covert_distance.py
@@ -0,0 +1,8 @@
+feet=float(input("Enter the distance in feet:"))
+inches = feet * 12
+yards = feet/3
+miles = feet/5280
+print(f"Distance in feet:{feet}")
+print(f"Distance in inches:{inches}")
+print(f"Distance in yards:{yards}")
+print(f"Distance in miles:{miles}")
